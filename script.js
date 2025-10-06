7c780571474f159ed168869d1c39e8f7cef1688d
MariaLuisa <marialuisap92@gmail.com>
2025-10-06 11:50:07 -0300

    UPDATE: Criação do Programa JS e Alteração fonte REM CSS

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Programa - Modelo 1 */
+window.document.addEventListener("DOMContentLoaded", function(){
+    /* Botão letras Maiúsculas */
+    window.document.querySelector("#btnMaiuscula").addEventListener("click",function(){
+        let inputText = document.querySelector("#input-text").value
+        document.querySelector("#result").innerHTML = inputText.toUpperCase()
+    })
+    /* Botão Letra Minúscula */
+    window.document.querySelector("#btnMinuscula").addEventListener("click",function(){
+        let inputText = document.querySelector("#input-text").value
+        document.querySelector("#result").innerHTML = inputText.toLowerCase()
+    })
+})
