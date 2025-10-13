552fb271674a1921171936d8e1518a668bd6e31e
maria luisa <user.email>
2025-10-13 15:09:16 -0300

    UPDATE, Personalização do site - HTML, JS, CSS.E adicionando logos site-IMG

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 /* Programa - Modelo 1 */
-window.document.addEventListener("DOMContentLoaded", function(){
-    /* Botão letras Maiúsculas */
-    window.document.querySelector("#btnMaiuscula").addEventListener("click",function(){
-        let inputText = document.querySelector("#input-text").value
-        document.querySelector("#result").innerHTML = inputText.toUpperCase()
-    })
-    /* Botão Letra Minúscula */
-    window.document.querySelector("#btnMinuscula").addEventListener("click",function(){
-        let inputText = document.querySelector("#input-text").value
-        document.querySelector("#result").innerHTML = inputText.toLowerCase()
-    })
-})
+window.document.addEventListener("DOMContentLoaded", function () {
+  /* Botão letras Maiúsculas */
+  window.document
+    .querySelector("#btnMaiuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result-textarea").innerHTML =
+        inputText.toUpperCase();
+    });
+  /* Botão Letra Minúscula */
+  window.document
+    .querySelector("#btnMinuscula")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      document.querySelector("#result-textarea").innerHTML =
+        inputText.toLowerCase();
+    });
+});
