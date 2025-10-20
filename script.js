67d6edb6e678e2e96e00f8a2f0b9aa7f133b10af
Maria Luísa <marialuisap92@gmail.com>
2025-10-20 11:52:08 -0300

    Update, Botão listar, HTML, CSS e JS

--- script.js
+++ script.js
@@ -24,11 +24,19 @@ window.document.addEventListener("DOMContentLoaded", function () {
       document.querySelector("#result-textarea").innerHTML =
         inputText.charAt(0).toUpperCase() + inputText.slice(1);
     });
-    // Botão Limpar
-    window.document
-      .querySelector("#btnLimpar")
-      .addEventListener("click", function () {
-        document.querySelector("#input-text").value = "";
-        document.querySelector("#result-textarea").innerHTML = "";
-      });
+  // Botão Limpar
+  window.document
+    .querySelector("#btnLimpar")
+    .addEventListener("click", function () {
+      document.querySelector("#input-text").value = "";
+      document.querySelector("#result-textarea").innerHTML = "";
+    });
+  // Botão listar
+  window.document
+    .querySelector("#btnListar")
+    .addEventListener("click", function () {
+      let inputText = document.querySelector("#input-text").value;
+      let palavras = inputText.split(" ");
+      document.querySelector("#result-textarea").innerHTML = palavras.join("\n");
+    });
 });
